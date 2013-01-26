2d6455efceb902824547acc6e4424efd288b7a1b
Bryan Green <twistedogre@gmail.com>
2013-01-25 21:43:22 -0800

    prod and dev keys

--- js/fitness.js
+++ js/fitness.js
@@ -664,13 +664,22 @@ var fitness = fitness || {
 };
 
 $(function() {
-    StackMob.init({
-        appName: "fitnesschallenge",
-        clientSubdomain: "twistedogregmailcom",
-        publicKey: "ba025b72-92db-4681-9abb-231baca5a94d",
-        apiVersion: 2
-    });
-
+    if (window.location.href.indexOf('stackmobapp.com') !== -1) {
+        StackMob.init({ // production
+            appName: "fitnesschallenge",
+            clientSubdomain: "twistedogregmailcom",
+            publicKey: "eb6421b3-9737-4f0b-97ce-8aebc448739b",
+            apiVersion: 2
+        });
+    }
+    else {
+        StackMob.init({ // dev
+            appName: "fitnesschallenge",
+            clientSubdomain: "twistedogregmailcom",
+            publicKey: "ba025b72-92db-4681-9abb-231baca5a94d",
+            apiVersion: 0
+        });
+    }
     var username = localStorage.getItem('username');
     if (username) {
         fitness.loginWithID(username, function() {
